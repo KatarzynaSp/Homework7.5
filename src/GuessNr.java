8e25ffac0e231e999ef922c2f2fbacebe3e332c5
Katarzyna <katarzyna.panek111@gmail.com>
2019-10-17 22:31:32 +0200

    zad 5 poprawka 1.

--- src/GuessNr.java
+++ src/GuessNr.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class GuessNr {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number = 0;
         do {
             System.out.println("Podaj liczbę");
-            int number = input.nextInt();
+            number = input.nextInt();
             if (number < 100) {
                 System.out.println("Podana liczba jest za mała");
             } else if (number > 200) {
                 System.out.println("Podana liczba jest za duża");
             } else if (number % 3 != 0) {
                 System.out.println("Liczba nie jest podzielna przez 3");
-            } else {
+            } else if (number>100 && number < 200 && number % 3 == 0) {
                 System.out.println("Twoja liczba jest ok");
-                break;
-            }
-        } while (true);
-        input.close();
+            } else input.close();
+        }
+        while (number < 100 || number > 200 || number % 3 != 0);
     }
 }
